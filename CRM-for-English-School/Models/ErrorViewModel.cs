ac9b5c6c5f684ee0ef773967dc183a3dfcedddfa
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-24 18:11:52 +0300

    M-01_Beginning of the project.

---
+++ CRM-for-English-School/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRM_for_English_School.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
