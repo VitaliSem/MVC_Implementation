83796d99ae7682a7bdfdd3fc0e195e0e7264a5c9
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-25 13:54:52 +0300

    The Interface for working with person entity was added.

--- CRM-for-English-School.BLL/DataTransferObjects/StudentDataTransferObject.cs
+++ CRM-for-English-School.BLL/DataTransferObjects/StudentDataTransferObject.cs
@@ -1,18 +1,13 @@
-﻿using CRM_for_English_School.DAL.Entities;
+﻿using CRM_for_English_School.BLL.DataTransferObjects;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CRM_for_English_School.BLL.DataTransportObjects
 {
-    class StudentDataTransferObject
+    class StudentDataTransferObject : PersonDataTransferObject
     {
         public int StudentID { get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public int Age { get; set; }
-        public Gender Gender { get; set; }
         public string CurrentEnglishLevel { get; set; }
     }
 }
---
+++ CRM-for-English-School.BLL/Interfaces/IPersonService.cs
@@ -0,0 +1,17 @@
+﻿using CRM_for_English_School.BLL.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_for_English_School.BLL.Interfaces
+{
+    interface IPersonService
+    {
+        void AddPerson(PersonDataTransferObject person);
+        void EditPerson(PersonDataTransferObject person);
+        void DeletePerson(int id);
+        PersonDataTransferObject GetWorker(int id);
+        IEnumerable<PersonDataTransferObject> GetPerson();
+        void Dispose();
+    }
+}
