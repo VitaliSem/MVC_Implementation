a75734d3b33df0814ed39612dddffe2c72c66bd6
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-25 12:49:51 +0300

    Interfaces for working with data in data bases were added.

---
+++ CRM-for-English-School.DAL/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using CRM_for_English_School.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_for_English_School.DAL.Interfaces
+{
+    interface IUnitOfWork
+    {
+        IWorkingProcess<Student> Students { get; }
+
+        void Save();
+    }
+}
