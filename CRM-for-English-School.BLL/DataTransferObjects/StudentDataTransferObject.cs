380b64404ab346cc1ee897477e6768ad3f7bc979
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-07-25 13:47:23 +0300

    DataTransferObject for an interaction between DAL and Wiev layer was added.

---
+++ CRM-for-English-School.BLL/DataTransferObjects/StudentDataTransferObject.cs
@@ -0,0 +1,18 @@
+﻿using CRM_for_English_School.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_for_English_School.BLL.DataTransportObjects
+{
+    class StudentDataTransferObject
+    {
+        public int StudentID { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set; }
+        public string CurrentEnglishLevel { get; set; }
+    }
+}
