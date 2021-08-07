c44d651e949821efe68d314fcff60c057314211d
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-08-07 20:23:41 +0300

    IBaseEntityService and BaseEntityService were added. Structure of model on BLL was simplified.

---
+++ CRM-for-English-School.BLL/Interfaces/IBaseEntityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM_for_English_School.BLL.Interfaces
+{
+    interface IBaseEntityService<TEntity>
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetEntity(int id);
+        void CreateEntity(TEntity entity);
+        void EditEntity(TEntity entity);
+        void DeleteEntity(int id);
+    }
+}
