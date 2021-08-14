e06855a2680a4d7243722e5323dd00cb62aa8dc0
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-08-14 19:22:25 +0300

    Add Teachers into EnglishScholl Database.

--- CRM-for-English-School.DAL.EF/Context/EnglishSchoolContext.cs
+++ CRM-for-English-School.DAL.EF/Context/EnglishSchoolContext.cs
@@ -13,7 +13,7 @@ namespace CRM_for_English_School.DAL.EF.Context
         public EnglishSchoolContext(DbContextOptions<EnglishSchoolContext> options) : base(options) { }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EnglishSchoolDB;Trusted_Connection=True;");
+            //optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EnglishSchoolDB;Trusted_Connection=True;");
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -22,12 +22,82 @@ namespace CRM_for_English_School.DAL.EF.Context
             modelBuilder.Entity<Student>().HasData(
                 new Student[]
                 {
-                    new Student { ID = 1, LastName = "Иванов", FirstName = "Иван", MiddleName = "Иванович", Age = 23, Gender = Gender.Male, CurrentEnglishLevel = "Elementary"},
-                    new Student { ID = 2, LastName = "Петров", FirstName = "Петр", MiddleName = "Петрович", Age = 28, Gender = Gender.Male, CurrentEnglishLevel = "Pre-Intermediate"},
-                    new Student { ID = 3, LastName = "Синичкина", FirstName = "Ольга", MiddleName = "Дмитриевна", Age = 24, Gender = Gender.Female, CurrentEnglishLevel = "Intermediate"},
-                    new Student { ID = 4, LastName = "Васнецов", FirstName = "Виктор", MiddleName = "Борисович", Age = 25, Gender = Gender.Male, CurrentEnglishLevel = "Intermediate"},
-                    new Student { ID = 5, LastName = "Прокофьева", FirstName = "Дарья", MiddleName = "Петровна", Age = 23, Gender = Gender.Female, CurrentEnglishLevel = "Elementary"},
+                    new Student 
+                    {
+                        ID = 1,
+                        LastName = "Иванов",
+                        FirstName = "Иван",
+                        MiddleName = "Иванович",
+                        Age = 23,
+                        Gender = Gender.Male,
+                        CurrentEnglishLevel = "Elementary"
+                    },
+                    new Student {
+                        ID = 2,
+                        LastName = "Петров",
+                        FirstName = "Петр",
+                        MiddleName = "Петрович",
+                        Age = 28,
+                        Gender = Gender.Male,
+                        CurrentEnglishLevel = "Pre-Intermediate"
+                    },
+                    new Student {
+                        ID = 3,
+                        LastName = "Синичкина",
+                        FirstName = "Ольга",
+                        MiddleName = "Дмитриевна",
+                        Age = 24,
+                        Gender = Gender.Female,
+                        CurrentEnglishLevel = "Intermediate"
+                    },
+                    new Student {
+                        ID = 4,
+                        LastName = "Васнецов",
+                        FirstName = "Виктор",
+                        MiddleName = "Борисович",
+                        Age = 25,
+                        Gender = Gender.Male,
+                        CurrentEnglishLevel = "Intermediate"
+                    },
+                    new Student {
+                        ID = 5,
+                        LastName = "Прокофьева",
+                        FirstName = "Дарья",
+                        MiddleName = "Петровна", 
+                        Age = 23,
+                        Gender = Gender.Female,
+                        CurrentEnglishLevel = "Elementary"
+                    },
+                });
 
+            modelBuilder.Entity<Teacher>().HasData(
+                new Teacher[]
+                {
+                    new Teacher
+                    {
+                        ID = 1,
+                        LastName = "Синицина",
+                        FirstName = "Светлана",
+                        MiddleName = "Викторовна",
+                        Age = 32,
+                        Gender = Gender.Female,
+                        GraduatedFrom = "MSLU",
+                        Degree = "Bachelor",
+                        MainSpecialization = "English language history"
+                    },
+                    new Teacher
+                    {
+                        ID = 2,
+                        LastName = "Кузнецова",
+                        FirstName = "Екатерина",
+                        MiddleName = "Степановна",
+                        Age = 37,
+                        Gender = Gender.Female,
+                        GraduatedFrom = "BSU",
+                        Degree = "Master",
+                        MainSpecialization = "Linguistics"
+
+                    }
                 });
         }
     }
