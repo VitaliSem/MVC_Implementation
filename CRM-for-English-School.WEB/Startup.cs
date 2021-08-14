6fc45ae8246b3eb10c819918810d0aa9509522a2
Vitali Semashko <vitalik.semashko.93@gmail.com>
2021-08-14 20:34:26 +0300

    Web Api was added. The project supports using Swagger.

---
+++ CRM-for-English-School.WEB/Startup.cs
@@ -0,0 +1,62 @@
+using CRM_for_English_School.BLL.Entities;
+using CRM_for_English_School.BLL.Interfaces;
+using CRM_for_English_School.BLL.Services;
+using CRM_for_English_School.DAL.EF.Context;
+using CRM_for_English_School.DAL.EF.Repositories;
+using CRM_for_English_School.DAL.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CRM_for_English_School.WEB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new OpenApiInfo { Title = "SwaggerWebApiApp", Version = "v1" }));
+
+            services.AddDbContext<EnglishSchoolContext>(options =>
+                options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=EnglishSchoolDB;Trusted_Connection=True;"));
+
+            services.AddScoped<IRepository<Teacher>, BaseEntityRepository<Teacher>>();
+
+            services.AddScoped<IBaseEntityService<Teacher>, BaseEntityService<Teacher>>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SwaggerWebApiApp v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
